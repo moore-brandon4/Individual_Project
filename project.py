c79a7456f68c4b77f5fc182d9348f0e019d30296
moore-brandon4 <moore_brandon4@columbusstate.edu>
2021-11-01 21:46:40 +0000

    templates done

---
+++ project.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template             # import flask framework
+app = Flask(__name__)             # create an app instance
+
+
+
+@app.route("/")                
+def gridbyexample():                    
+    return render_template("gridbyexample.html") 
+
+@app.route("/zachholman")                
+def zachholman():                    
+    return render_template("zachholman.html") 
+
+@app.route("/harrypotter")                
+def harrypotter():                    
+    return render_template("harrypotter.html")           
+
+app.run(debug=True)
